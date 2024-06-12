7ed2aaae80570a1fa1736e63e0898f33a85eac05
@iamserda <iamserda@gmail.com>
2024-06-12 00:48:46 -0400

    minor: moved db_seeding functionality to db_handler, fixed issue where display terminates early. -@iamserda

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sqlite3
 import json
 from display_menu import show_app_menu
-from db_handlers import create_db,create_boxes_table
-from models import Box
-
+from db_handlers import create_db,create_boxes_table,\
+                        add_box_to_db, add_boxes_to_db\
 
 
 
@@ -43,45 +42,6 @@ from models import Box
 # while FLAG:
 #     FLAG = show_app_menu()
 
-
-def add_box_to_db(box:Box, db_connector:sqlite3.Connection)->None:
-    try:
-        db_cursor = db_connector.cursor()
-        statement = """
-        INSERT INTO boxes 
-        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);
-        """
-        new_box = box.__dict__
-        db_cursor.execute(statement, new_box)
-        db_connector.commit()
-        db_cursor.close()
-    except sqlite3.OperationalError as err:
-        print(err)
-    except sqlite3.IntegrityError as err:
-        print(err)
-
-def add_boxes_to_db(boxes:list,db_connector:sqlite3.Connection)->None:
-    try:
-        db_cursor = db_connector.cursor()
-        statement = """
-        INSERT INTO boxes 
-        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);
-        """
-        db_cursor.executemany(statement, boxes)
-        db_connector.commit()
-        db_cursor.close()
-    except sqlite3.OperationalError as err:
-        print(err)
-    except sqlite3.IntegrityError as err:
-        print(err)
-
-def seed_db(records, db_conn):
-    for record in records:
-        del record["id"]
-        del record["container_id"]
-    boxes = [box.__dict__ for box in [Box(**record) for record in records]]
-    add_boxes_to_db(boxes=boxes, db_connector=db_conn)
-
 if __name__ == "__main__":
     
     # creates db, returns connection obj.
@@ -94,6 +54,12 @@ if __name__ == "__main__":
     # seed_db(records, db_conn)
     # create
     # run menu
-    show_app_menu()
-
+    display_menu_flag = True
+    while display_menu_flag:
+        display_menu_flag = show_app_menu()
+    
+    # Use to seed DB first-time
+    # seed_db(db_conn)
+    
     db_conn.close()
+    
