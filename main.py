14a4d3b74b875f7e0ec01e27581695889b3da06f
@iamserda <iamserda@gmail.com>
2024-06-11 23:35:36 -0400

    First setup of main, moved some handlers functions to an external file for later import as modules. -@iamserda

--- main.py
+++ main.py
@@ -1,16 +1,97 @@
 import sqlite3
 import json
-
-db_connector = sqlite3.connect("project1.db")
-db_cursor = db_connector.cursor()
-
-
-# class box:
-# class container
-# class order
+from display_menu import show_app_menu
+from db_handlers import create_db,create_boxes_table
+from models import Box
 
 
-# menu:
-# - create and ship a box
+
+
+
+
+
+# # class container
+# try:
+    # db_cursor.execute(
+    #     "CREATE TABLE containers"
+    # )
+    # # class order
+    # db_cursor.execute(
+    #     "CREATE TABLE orders"
+    # )
+# except sqlite3.OperationalError as err:
+    # print(f"Error: {err} creating Boxes table!")
+    # pass 
+
+
+# # class freight
+# try:
+#     db_cursor.execute(
+#         "CREATE TABLE freights"
+#     )
+# except sqlite3.OperationalError as err:
+#     print(f"Error: {err} creating Boxes table!")
+#     pass
+
+
 # - load box into containers
 # - see revenue, expense, projected profit
+
+# MENU
+# FLAG = True
+
+# while FLAG:
+#     FLAG = show_app_menu()
+
+
+def add_box_to_db(box:Box, db_connector:sqlite3.Connection)->None:
+    try:
+        db_cursor = db_connector.cursor()
+        statement = """
+        INSERT INTO boxes 
+        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);
+        """
+        new_box = box.__dict__
+        db_cursor.execute(statement, new_box)
+        db_connector.commit()
+        db_cursor.close()
+    except sqlite3.OperationalError as err:
+        print(err)
+    except sqlite3.IntegrityError as err:
+        print(err)
+
+def add_boxes_to_db(boxes:list,db_connector:sqlite3.Connection)->None:
+    try:
+        db_cursor = db_connector.cursor()
+        statement = """
+        INSERT INTO boxes 
+        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);
+        """
+        db_cursor.executemany(statement, boxes)
+        db_connector.commit()
+        db_cursor.close()
+    except sqlite3.OperationalError as err:
+        print(err)
+    except sqlite3.IntegrityError as err:
+        print(err)
+
+if __name__ == "__main__":
+    
+    # creates db, returns connection obj.
+    db_conn = create_db()
+
+    #using connection obj
+    create_boxes_table(db_connector=db_conn)
+    # create_freights_table(db_connector=db_conn)
+    # create_containers_table(db_connector=db_conn)
+
+    # create
+    # run menu
+    # show_app_menu()
+    from db.data_import import records
+    for record in records:
+        del record["id"]
+        del record["container_id"]
+    boxes = [box.__dict__ for box in [Box(**record) for record in records]]
+    add_boxes_to_db(boxes=boxes, db_connector=db_conn)
+    db_conn.close()
