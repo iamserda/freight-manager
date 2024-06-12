1d34dd826b1cb0bc1e4e318db0c8172f41a50863
@iamserda <iamserda@gmail.com>
2024-06-12 00:46:48 -0400

    minor: moved db seeders to db_handler module

--- db_handlers.py
+++ db_handlers.py
@@ -1,4 +1,7 @@
 import sqlite3
+from models import Box
+from db.data_import import records
+
 
 
 def create_db():
@@ -66,3 +69,43 @@ def create_boxes_table(db_connector:sqlite3.Connection):
 #     except sqlite3.OperationalError as err:
 #         print(f"Error: {err} creating Boxes table!")
 #         pass
+
+def add_box_to_db(box:Box, db_connector:sqlite3.Connection)->None:
+    try:
+        db_cursor = db_connector.cursor()
+        statement = """
+        INSERT INTO boxes 
+        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);
+        """
+        new_box = box.__dict__
+        db_cursor.execute(statement, new_box)
+        db_connector.commit()
+        db_cursor.close()
+    except sqlite3.OperationalError as err:
+        print(err)
+    except sqlite3.IntegrityError as err:
+        print(err)
+
+def add_boxes_to_db(boxes:list,db_connector:sqlite3.Connection)->None:
+    try:
+        db_cursor = db_connector.cursor()
+        statement = """
+        INSERT INTO boxes 
+        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);
+        """
+        db_cursor.executemany(statement, boxes)
+        db_connector.commit()
+        db_cursor.close()
+    except sqlite3.OperationalError as err:
+        print(err)
+    except sqlite3.IntegrityError as err:
+        print(err)
+
+def seed_db(db_conn):
+    
+    for record in records:
+        del record["id"]
+        del record["container_id"]
+    boxes = [box.__dict__ for box in [Box(**record) for record in records]]
+    add_boxes_to_db(boxes=boxes, db_connector=db_conn)
+
