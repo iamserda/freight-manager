76eb5832f3df246fb5bd4ef4d59b357829aa5433
@iamserda <iamserda@gmail.com>
2024-06-12 00:47:14 -0400

    minor changes to display module.

--- display_menu.py
+++ display_menu.py
@@ -1,13 +1,13 @@
 """ User Interface: prints menu full options to end user. """
 def show_app_menu():
     """
-# menu: a menue to be displayed until user terminates
-# -- 1. add box type
-# -- 2. Show box types
-# -- 3. Load box into a given container
-# -- 4. Show Containers
-# -- 5. Summary Report
-# -- x. Close Menu
+menu: a menue to be displayed until user terminates
+-- 1. add box type
+-- 2. Show box types
+-- 3. Load box into a given container
+-- 4. Show Containers
+-- 5. Summary Report
+-- x. Close Menu
 """
     menu = """
     MENU:
