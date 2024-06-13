53273e617c8fa92568047779874a2b1ef21c8c0c
@iamserda <iamserda@gmail.com>
2024-06-12 23:56:26 -0400

    minor: simplifed module, removed commented code, cleansed -@iamserda

--- main.py
+++ main.py
@@ -1,65 +1,12 @@
-import sqlite3
-import json
 from display_menu import show_app_menu
-from db_handlers import create_db,create_boxes_table,\
-                        add_box_to_db, add_boxes_to_db\
-
-
-
-
-
-
-# # class container
-# try:
-    # db_cursor.execute(
-    #     "CREATE TABLE containers"
-    # )
-    # # class order
-    # db_cursor.execute(
-    #     "CREATE TABLE orders"
-    # )
-# except sqlite3.OperationalError as err:
-    # print(f"Error: {err} creating Boxes table!")
-    # pass 
-
-
-# # class freight
-# try:
-#     db_cursor.execute(
-#         "CREATE TABLE freights"
-#     )
-# except sqlite3.OperationalError as err:
-#     print(f"Error: {err} creating Boxes table!")
-#     pass
-
-
-# - load box into containers
-# - see revenue, expense, projected profit
-
-# MENU
-# FLAG = True
-
-# while FLAG:
-#     FLAG = show_app_menu()
+from db_handlers import create_db,create_table, seed_db
+from db.data_import import records
 
 if __name__ == "__main__":
-    
     # creates db, returns connection obj.
     db_conn = create_db()
-
-    #using connection obj
-    create_boxes_table(db_connector=db_conn)
-    # create_freights_table(db_connector=db_conn)
-    # create_containers_table(db_connector=db_conn)
-    # seed_db(records, db_conn)
-    # create
-    # run menu
-    display_menu_flag = True
-    while display_menu_flag:
-        display_menu_flag = show_app_menu()
-    
-    # Use to seed DB first-time
-    # seed_db(db_conn)
-    
+    create_table(db_connector=db_conn, sql_="boxes")
+    create_table(db_connector=db_conn, sql_="freights")
+    create_table(db_connector=db_conn, sql_="containers")
+    seed_db(records, db_conn)
     db_conn.close()
-    
