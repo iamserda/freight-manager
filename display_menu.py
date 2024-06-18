25c77e0d5095747c19b218d774541adaa0bf638a
@iamserda <iamserda@gmail.com>
2024-06-17 21:43:25 -0400

    minor: simplified comments. -@iamserda

--- display_menu.py
+++ display_menu.py
@@ -1,30 +1,21 @@
 """ User Interface: prints menu full options to end user. """
 def show_app_menu():
-    """
-menu: a menue to be displayed until user terminates
--- 1. add box type
--- 2. Show box types
--- 3. Load box into a given container
--- 4. Show Containers
--- 5. Summary Report
--- x. Close Menu
-"""
-    menu = """
-    MENU:
-1. Add box type
-2. Show box types
-3. Load box into a given container
-4. Show Containers
-5. Summary Report
-x. Close Menu\n
-$: """
-
+    """menu: a menu to be displayed"""
+    menu = """MENU:
+    1. Add box type
+    2. Show box types
+    3. Load box into a given container
+    4. Show Containers
+    5. Summary Report
+    x. Close Menu\n
+    $: """
+    
+    valid_option = range(1, 6)
     choice = input(menu).strip()
     if choice.casefold() == "x":
         print("Terminating application at user's request...")
         return False
-    if choice not in ["1","2","3","4","5"]:
+    if int(choice) not in valid_option:
         print(f"Selection-Error: {choice} is NOT a valid option. Please see the menu and try again!")
-    else:
-        print(f"Selected option: {choice}")
-    return True
+        return True
+    return int(choice)
