5aa2d93debeea0338443a9056c876fccca1cd941
@iamserda <iamserda@gmail.com>
2024-06-17 21:42:41 -0400

    fully implemented create table functionality. renamed some functions see: insert_box func, insert_boxes func, seed_boxes_table func. Added seed_freights_table func, show_box_types, and insert_freights. -@iamserda

--- db_handlers.py
+++ db_handlers.py
@@ -3,64 +3,71 @@ from models import Box
 
 
 def create_db():
-    db_connector = sqlite3.connect("./db/base1.db")
-    # class box:
-    return db_connector
+    """
+    Creates database file if it does not exits,
+    creates a connection to the database.
+    return the newly created connection.
+    """
+    db_connection = sqlite3.connect("./db/base1.db")
+    return db_connection
 
 
 def create_table(db_connector: sqlite3.Connection, sql_: str):
+    """Creates all tables for the app."""
     sql_statements = {
-        "boxes": """CREATE TABLE Boxes(
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            weight REAL NOT NULL,
-            width REAL NOT NULL,
-            height REAL NOT NULL,
-            length REAL NOT NULL,
-            container_id INTEGER NOT NULL REFERENCES Containers(id) ON DELETE CASCADE,
-            CONSTRAINT max_volume CHECK (width * length * height <= 50),
-            CONSTRAINT max_weight CHECK (weight <= 50));""",
-        "freights": """CREATE TABLE Freights(
-            id INTEGER PRIMARY KEY,
-            container_id INTEGER NOT NULL,
-            box_id INTEGER NOT NULL REFERENCES Boxes(id) On DELETE CASCADE);""",
-        "containers": """CREATE TABLE Containers(
-            id INTEGER PRIMARY KEY,
-            container_id INTEGER NOT NULL,
-            box_id INTEGER NOT NULL REFERENCES Boxes(id) On DELETE CASCADE);""",
-        }
+        "boxes": """
+        CREATE TABLE Boxes(
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        weight REAL NOT NULL,
+        width REAL NOT NULL,
+        height REAL NOT NULL,
+        length REAL NOT NULL,
+        container_id INTEGER NOT NULL DEFAULT 1 REFERENCES Containers(id) ON DELETE CASCADE,
+        CONSTRAINT max_volume CHECK (width * length * height <= 50),
+        CONSTRAINT max_weight CHECK (weight <= 50));""",
+        
+        "freights": """
+        CREATE TABLE Freights(
+        id INTEGER PRIMARY KEY,
+        container_id INTEGER NOT NULL DEFAULT 1,
+        box_id INTEGER NOT NULL REFERENCES boxes(id) ON DELETE CASCADE);"""
+    }
 
     try:
-        print(sql_statements[sql_][:19])
-        # db_cursor = db_connector.cursor()
-        # db_cursor.execute(sql_statements[sql_])
-        # db_cursor.close()
+        db_cursor = db_connector.cursor()
+        db_cursor.execute(f"DROP TABLE IF EXISTS {sql_};")
+        db_cursor.execute(sql_statements[sql_])
+        db_cursor.close()
     except sqlite3.OperationalError as err:
-        print(f"Error: {err} creating Boxes table!")
+        print(f"Error: {err} creating {sql_} table!")
 
-def add_box(box: Box.__dict__, db_connector: sqlite3.Connection) -> None:
+
+def insert_box(box:Box, db_connector: sqlite3.Connection) -> None:
     """Given a box as a dictionary, it adds a new box to the db.
     Prints any error to the console."""
     try:
         db_cursor = db_connector.cursor()
-        statement = """INSERT INTO boxes VALUES(:id, :name, :weight, :width, :length, :height, :container_id);"""
-        db_cursor.execute(statement, box)
+        statement = """
+        INSERT INTO Boxes 
+        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);"""
+        db_cursor.execute(statement, box.__dict__)
         db_connector.commit()
         db_cursor.close()
     except sqlite3.OperationalError as err:
-        print(err)
+        raise err
     except sqlite3.IntegrityError as err:
-        print(err)
+        raise err
 
 
-def add_boxes(boxes: list, db_connector: sqlite3.Connection) -> None:
+def insert_boxes(boxes: list, db_connector: sqlite3.Connection) -> None:
     """Given an list of boxes, adds all these boxes to the table.
     If a box already exist, it prints out an error!"""
     try:
         db_cursor = db_connector.cursor()
         statement = """
-        INSERT INTO boxes 
-        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);
+        INSERT INTO Boxes 
+        VALUES(:id,:name, :weight, :width, :length, :height, :container_id);
         """
         db_cursor.executemany(statement, boxes)
         db_connector.commit()
@@ -71,14 +78,63 @@ def add_boxes(boxes: list, db_connector: sqlite3.Connection) -> None:
         print(err)
 
 
-def seed_db(records:list, db_conn):
-    """Creates a list of dictionaries representing boxes, creates a list of Boxes objects, converts them to a dictionary, and add them to db."""
+def seed_boxes_table(records: list, db_conn):
+    """
+        Creates a list of dictionaries representing boxes,
+        creates a list of Boxes objects,
+        converts them to a dictionary, and add them to db.
+    """
     try:
-        for record in records:
-            del record["id"]
-            del record["container_id"]
-        boxes = [box.__dict__ for box in [Box(**record) for record in records]]
-        add_boxes(boxes=boxes, db_connector=db_conn)
+        insert_boxes(boxes=records, db_connector=db_conn)
     except Exception as err:
         print(f"Error: Attempt to seed db failed...\n{err}")
         raise err
+
+def insert_freight(box:dict, db_connector: sqlite3.Connection) -> None:
+    """Given a box as a dictionary, it adds a new box to the db.
+    Prints any error to the console."""
+    try:
+        db_cursor = db_connector.cursor()
+        statement = """
+        INSERT INTO Freights VALUES(:id,:container_id, :box_id);"""
+        db_cursor.execute(statement, box)
+        db_connector.commit()
+        db_cursor.close()
+    except sqlite3.OperationalError as err:
+        print(err)
+    except sqlite3.IntegrityError as err:
+        print(err)
+
+
+def insert_freights(boxes: list, db_connector: sqlite3.Connection) -> None:
+    """Given an list of freights records, adds all these freights to the table.
+    If a freight already exist, it prints out an error!"""
+    try:
+        db_cursor = db_connector.cursor()
+        statement = """INSERT INTO Boxes 
+        VALUES(:id,:name, :weight, :width, :length, :height, :container_id);"""
+        db_cursor.executemany(statement, boxes)
+        db_connector.commit()
+        db_cursor.close()
+    except sqlite3.OperationalError as err:
+        print(err)
+    except sqlite3.IntegrityError as err:
+        print(err)
+
+
+def seed_freights_table(records: list, db_conn):
+    """
+        Creates a list of dictionaries representing boxes,
+        creates a list of Boxes objects,
+        converts them to a dictionary, and add them to db.
+    """
+    try:
+        insert_boxes(boxes=records, db_connector=db_conn)
+    except Exception as err:
+        print(f"Error: Attempt to seed db failed...\n{err}")
+        raise err
+
+def show_box_types(box_type, db_conn:sqlite3.Connection):
+    sql_statement = """SELECT * FROM boxes WHERE(:name)"""
+    box_types_data = db_conn.execute(sql_statement, box_type).fetchall()
+    return box_types_data
