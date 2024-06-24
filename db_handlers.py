980682db9ec172fbeb1bfb40588b11bd63c93b27
@iamserda <iamserda@gmail.com>
2024-06-24 00:37:18 -0400

    modified handlers to only create tables if they don't already exist, removed container_id as a DB column in boxes table, fixed foreign_key issue in freights table, some code cleanup, implemented/improved show_box_by_name functionality, and added get_container by id functionality. -@iamserda

--- db_handlers.py
+++ db_handlers.py
@@ -16,22 +16,24 @@ def create_table(db_connector: sqlite3.Connection, sql_: str):
     """Creates all tables for the app."""
     sql_statements = {
         "boxes": """
-        CREATE TABLE Boxes(
+        CREATE TABLE IF NOT EXISTS Boxes(
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
         weight REAL NOT NULL,
         width REAL NOT NULL,
         height REAL NOT NULL,
         length REAL NOT NULL,
-        container_id INTEGER NOT NULL DEFAULT 1 REFERENCES Containers(id) ON DELETE CASCADE,
-        CONSTRAINT max_volume CHECK (width * length * height <= 50),
-        CONSTRAINT max_weight CHECK (weight <= 50));""",
+        CONSTRAINT max_volume CHECK (width * length * height <= 50));
+        """,
         
         "freights": """
-        CREATE TABLE Freights(
-        id INTEGER PRIMARY KEY,
-        container_id INTEGER NOT NULL DEFAULT 1,
-        box_id INTEGER NOT NULL REFERENCES boxes(id) ON DELETE CASCADE);""",
+        CREATE TABLE IF NOT EXISTS Freights (
+            id INTEGER PRIMARY KEY,
+            container_id INTEGER NOT NULL DEFAULT 1,
+            box_id INTEGER NOT NULL,
+            FOREIGN KEY(box_id) REFERENCES Boxes(id) ON DELETE CASCADE
+        );
+        """,
         
         "container_view": """
         CREATE VIEW IF NOT EXISTS containers
@@ -51,6 +53,8 @@ def create_table(db_connector: sqlite3.Connection, sql_: str):
         db_cursor.close()
     except sqlite3.OperationalError as err:
         print(f"Error: {err} creating {sql_} table!")
+    except Exception as err:
+        print(err)
 
 
 def insert_box(box:Box, db_connector: sqlite3.Connection) -> None:
@@ -58,9 +62,7 @@ def insert_box(box:Box, db_connector: sqlite3.Connection) -> None:
     Prints any error to the console."""
     try:
         db_cursor = db_connector.cursor()
-        statement = """
-        INSERT INTO Boxes 
-        VALUES(:id, :name, :weight, :width, :length, :height, :container_id);"""
+        statement = """INSERT INTO Boxes VALUES(:id, :name, :weight, :width, :length, :height);"""
         db_cursor.execute(statement, box.__dict__)
         db_connector.commit()
         db_cursor.close()
@@ -75,10 +77,7 @@ def insert_boxes(boxes: list, db_connector: sqlite3.Connection) -> None:
     If a box already exist, it prints out an error!"""
     try:
         db_cursor = db_connector.cursor()
-        statement = """
-        INSERT INTO Boxes 
-        VALUES(:id,:name, :weight, :width, :length, :height, :container_id);
-        """
+        statement = """INSERT INTO Boxes VALUES(:id,:name, :weight, :width, :length, :height);"""
         db_cursor.executemany(statement, boxes)
         db_connector.commit()
         db_cursor.close()
@@ -105,15 +104,16 @@ def insert_freight(box:dict, db_connector: sqlite3.Connection) -> None:
     Prints any error to the console."""
     try:
         db_cursor = db_connector.cursor()
-        statement = """
-        INSERT INTO Freights VALUES(:id,:container_id, :box_id);"""
-        db_cursor.execute(statement, box)
+        statement = """INSERT INTO Freights(container_id, box_id) VALUES(:container_id, :box_id);"""
+        relevant_box_data = box["container_id"],box["id"]
+        db_cursor.execute(statement, relevant_box_data)
         db_connector.commit()
         db_cursor.close()
+        return 1
     except sqlite3.OperationalError as err:
-        print(err)
+        raise err
     except sqlite3.IntegrityError as err:
-        print(err)
+        raise err
 
 
 def insert_freights(freights: list, db_connector: sqlite3.Connection) -> None:
@@ -131,7 +131,7 @@ def insert_freights(freights: list, db_connector: sqlite3.Connection) -> None:
         print(err)
 
 
-def seed_freights_table(freights:list, db_conn):
+def seed_freights_table(freights:list, db_conn)->None:
     """
         Creates a list of dictionaries representing boxes,
         creates a list of Boxes objects,
@@ -143,7 +143,32 @@ def seed_freights_table(freights:list, db_conn):
         print(f"Error: Attempt to seed db failed...\n{err}")
         raise err
 
-def show_box_types(box_type, db_conn:sqlite3.Connection):
-    sql_statement = """SELECT * FROM boxes WHERE(:name)"""
-    box_types_data = db_conn.execute(sql_statement, box_type).fetchall()
-    return box_types_data
+def show_box_by_name(name, db_conn:sqlite3.Connection)->tuple | None:
+    """Getter: Returns a specific box from the database given a name as a tuple. If none is found, logs error, return None"""
+    try:
+        my_cursor = db_conn.cursor()
+        sql_statement = """SELECT * FROM boxes WHERE(name=:name)"""
+        box_data:tuple = my_cursor.execute(sql_statement,(name,)).fetchone()
+        my_cursor.close()
+        if box_data is None:
+            raise LookupError(f"An item with name{box_data} was not found in our database.\n \
+        #                     Please check the name and try agains")
+        return box_data
+    except sqlite3.Error as sql_err:
+        print(sql_err) # fake logging
+    except LookupError as err:
+        print(err) # fake logging
+
+
+def get_container(container_id, db_conn:sqlite3.Connection)->tuple | None:
+    """Getter: Given a container ID, query the database for the specified container, returns related data. Returns None if not found."""
+    try:
+        db_cursor = db_conn.cursor()
+        sql_statement = "SELECT * FROM containers WHERE(container_id=:container_id)"
+        container:tuple = db_cursor.execute(sql_statement, (container_id,)).fetchone()
+        db_cursor.close()
+        if container is None:
+            raise LookupError(f"A container with the following ID: '{container_id}' was NOT found in the database.")
+        return container
+    except Exception as err:
+        raise err
